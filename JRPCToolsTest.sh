2e0c5067046ff6ffa6cbdb5668a2456c05757248
Matt Flax <flatmax@flatmax.com>
2022-09-06 14:06:41 +1000

    cleanup JRPCToolsTest.sh

--- JRPCToolsTest.sh
+++ JRPCToolsTest.sh
@@ -3,9 +3,10 @@ rm -rf cert.client; mkdir -p cert.client
 openssl req -newkey rsa:2048 -new -nodes -x509 -days 365 -keyout cert.client/key.pem -out cert.client/cert.pem
 rm -rf cert.server; mkdir -p cert.server; ln -s cert.server cert
 openssl req -newkey rsa:2048 -new -nodes -x509 -days 365 -keyout cert.server/server.key -out cert.server/server.crt
-./install.sh
+
 echo
-echo don\'t forget to run ./JRPCToolsTest.js
+echo Don\'t forget to run ./JRPCToolsTest.js
+echo Don\'t forget to open https://0.0.0.0:9000 in the browser to clear security issues as we are using wss with locally generated certs.
 echo
 polymer serve -P https/1.1 --hostname 0.0.0.0 --key cert.client/key.pem --cert cert.client/cert.pem
 
